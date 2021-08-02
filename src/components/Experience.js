f2aa9a75aa82a81f8355fea01b7f245f65eeaa7f
sharjeelyunus <sharjeel924@gmail.com>
2021-08-02 20:17:16 +0500

    👌 IMPROVE: Experience Section

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,17 +3,68 @@ import './Experience.css';
 
 const Experience = () => {
     return (
-        <div className="experience">
+        <div className="experience__section">
             <h1>Experience</h1>
-            <div className="experience__card">
-                <h2>Joint Secretary</h2>
-                <h3>Student's Psycology Society - SPS</h3>
-                <p>Feb 2021 - Present</p>
-            </div>
-            <div className="experience__card">
-                <h2>General Secretary</h2>
-                <h3>Green Development Program - GDP</h3>
-                <p>April 2021 - Present</p>
+            <div className="experience">
+                <div>
+                    <div className="experience__card">
+                        <h2>Joint Secretary</h2>
+                        <h3>Student's Psycology Society - SPS</h3>
+                        <p>Feb 2021 - Present</p>
+                    </div>
+                    <div className="experience__card">
+                        <h2>General Secretary</h2>
+                        <h3>Green Development Program - GDP</h3>
+                        <p>June 2021 - Present</p>
+                    </div>
+                    <div className="experience__card">
+                        <h2>Volunteer Membership</h2>
+                        <h3>International Fedration of Red Corss - IFRC</h3>
+                        <p>June 2020 - Present</p>
+                    </div>
+                    <div className="experience__card">
+                        <h2>Member</h2>
+                        <h3>Pakistan Red Cerscent Society - PRCS</h3>
+                        <p>June 2020 - Present</p>
+                    </div>
+                    <div className="experience__card">
+                        <h2>Member - Pakistan Chapter</h2>
+                        <h3>Youth Co-Lab</h3>
+                        <p>May 2021 - Present</p>
+                    </div>
+                    <div className="experience__card">
+                        <h2>Internship</h2>
+                        <h3>World Wildelife Fund</h3>
+                        <p>Jun 2021 - July 2021</p>
+                    </div>
+                </div>
+                <div>
+                    <div className="experience__card">
+                        <h2>Peace Ambassador</h2>
+                        <h3>Sustainable Social Development Organization</h3>
+                        <p>July 2021 - Present</p>
+                    </div>
+                    <div className="experience__card">
+                        <h2>Volunteer Membership</h2>
+                        <h3>United Nations</h3>
+                        <p>Jun 2021 - Present</p>
+                    </div>
+                    <div className="experience__card">
+                        <h2>Member</h2>
+                        <h3>Amnesty International</h3>
+                        <p>Jun 2021 - Present</p>
+                    </div>
+                    <div className="experience__card">
+                        <h2>Regional Supervisor</h2>
+                        <h3>Sustainable Development Policy Institute</h3>
+                        <p>Feb 2020 - Oct 2020</p>
+                    </div>
+                    <div className="experience__card">
+                        <h2>Volunteer</h2>
+                        <h3>OxFam International</h3>
+                        <p>Jan 2021 - April 2021</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
